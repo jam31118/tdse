3132ddc4a5620be646129d43d4be52194d35e46d
jam31118 <jam45@naver.com>
2018-06-10 20:51:59 +0900

    implemented 1d finite difference kernel convolution method

--- tdse/finite_difference.py
+++ tdse/finite_difference.py
@@ -58,3 +58,59 @@ def get_m_values_of_big_o(fx, dfx_ana, delta_x, zero_boundary=True, fd_approxima
     m_array = np.abs(dfx_ana - dfx_num) / (delta_x ** fd_approximation_order)
     return m_array
 
+
+def fd_diff_1d(f0_ana, kernel_center_unflipped, kernel_left_unflipped, kernel_right_unflipped):
+    """
+    Apply finite-difference approximation kernel on given array to calculate derivative corresponding to the given kernels.
+    """
+
+#    #### Prepare finite difference convolution kernels
+#
+#    # centered kernel
+#    kernel_unflipped = kernel_center_unflipped
+#    kernel = np.flip(kernel_unflipped, axis=0)
+#
+#    # one-sided kernel
+#    kernel_left_unflipped = kernel_left_unflipped
+#    kernel_left = np.flip(kernel_left_unflipped, axis=0)
+#    kernel_right = kernel_left_unflipped
+#
+
+    kernels_unflipped = [
+            kernel_center_unflipped, 
+            kernel_left_unflipped, 
+            kernel_right_unflipped
+    ]
+
+    for arg_array in kernels_unflipped + [f0_ana]:
+        assert isinstance(arg_array, np.ndarray)
+        assert arg_array.ndim == 1
+
+    kernel_center, kernel_left, kernel_right = (
+        np.flip(kernel_unflipped, axis=0) for kernel_unflipped 
+        in kernels_unflipped
+    )
+
+    kernel_center_length = kernel_center.size
+    kernel_left_length = kernel_left.size
+    kernel_right_length = kernel_right.size
+
+    assert (kernel_center_length % 2) == 1
+    assert f0_ana.size >= max(kernel_center_length, kernel_left_length, kernel_right_length)
+
+    kernel_center_half_width = kernel_center_length // 2  # excluding center point
+    valid_center_slice = slice(kernel_center_half_width, -kernel_center_half_width)
+
+    f1_num = np.empty_like(f0_ana)
+
+    # fill in center parts
+    f1_num[valid_center_slice] = np.convolve(f0_ana, kernel_center, mode='valid')
+
+    # fill in edges (boundaries)
+    assert kernel_left_length == kernel_right_length
+    one_sided_slice_size = kernel_center_half_width + kernel_left_length - 1
+    f1_num[:kernel_center_half_width] = np.convolve(f0_ana[:one_sided_slice_size], kernel_left, mode='valid')
+    f1_num[-kernel_center_half_width-1:] = np.convolve(f0_ana[-one_sided_slice_size-1:], kernel_right, mode='valid')
+    
+    return f1_num
+
