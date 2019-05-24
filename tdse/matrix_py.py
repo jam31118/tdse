4e5e5218db7f7fa764238e2c79f488dc4a526026
Ahn, Seongjin <jam45@naver.com>
2019-05-24 19:39:20 +0900

    moved tridiag function to tridiag module

--- tdse/matrix_py.py
+++ tdse/matrix_py.py
@@ -63,21 +63,4 @@ def mat_vec_mul_tridiag(alpha, beta, gamma, v, N=None):
     return b
 
 
-## Define macro function for tridiag datatype
-def tridiag_forward(tridiag, v, b):
-    """
-    The diagonal elements should be tridiag[1,:],
-    the lower offdiagonal should be tridiag[0,1:],
-    the upper offdiagonal should be tridiag[2,:-1]
-    """
-    b[:] = mat_vec_mul_tridiag(tridiag[1,:], tridiag[0,1:], tridiag[2,:-1], v)
-
-def tridiag_backward(tridiag, v, b):
-    """
-    The diagonal elements should be tridiag[1,:],
-    the lower offdiagonal should be tridiag[0,1:],
-    the upper offdiagonal should be tridiag[2,:-1]
-    """
-    v[:] = gaussian_elimination_tridiagonal(tridiag[1,:], tridiag[0,1:], tridiag[2,:-1], b)
-
 
---
+++ tdse/tridiag.py
@@ -0,0 +1,19 @@
+from .matrix import mat_vec_mul_tridiag, gaussian_elimination_tridiagonal
+
+## Define macro function for tridiag datatype
+def tridiag_forward(tridiag, v, b):
+    """
+    The diagonal elements should be tridiag[1,:],
+    the lower offdiagonal should be tridiag[0,1:],
+    the upper offdiagonal should be tridiag[2,:-1]
+    """
+    b[:] = mat_vec_mul_tridiag(tridiag[1,:], tridiag[0,1:], tridiag[2,:-1], v)
+
+def tridiag_backward(tridiag, v, b):
+    """
+    The diagonal elements should be tridiag[1,:],
+    the lower offdiagonal should be tridiag[0,1:],
+    the upper offdiagonal should be tridiag[2,:-1]
+    """
+    v[:] = gaussian_elimination_tridiagonal(tridiag[1,:], tridiag[0,1:], tridiag[2,:-1], b)
+
