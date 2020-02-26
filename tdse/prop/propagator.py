5cdf433bc73d4ac0b474d3fd7efe86da0339801c
Ahn, Seongjin <jam45@naver.com>
2020-02-26 22:40:25 +0900

    movement

--- tdse/prop/propagator.py
+++ tdse/prop/propagator.py
@@ -1,34 +1,27 @@
 """Module for wavefunction propagator"""
 
-from numbers import Integral
+# A propagator for the wavefunction in a one-dimensional box
+
+def norm_sq_on_uniform_grid_1d_box(wf, dx):
+    _abs_sq_wf = np.real(np.conj(wf) * wf)
+    return dx * np.sum(_abs_sq_wf)
+
+def normalize_on_uniform_grid_1d_box(wf, dx):
+    _norm_sq = norm_sq_on_uniform_grid_1d_box(wf, dx)
+    wf[:] *= 1. / np.sqrt(_norm_sq)
+
+
+from numbers import Integral, Real
 
 import numpy as np
 
-from ..evol import get_D2_tridiag, get_M2_tridiag, mul_tridiag_and_diag
-from ..tridiag import tridiag_forward, tridiag_backward
+from tdse.evol import get_D2_tridiag, get_M2_tridiag, mul_tridiag_and_diag
+from tdse.tridiag import tridiag_forward, tridiag_backward
 
 class Time_Indep_Hamil_Propagator(object):
     """A Propagator for a time-independent Hamiltonian"""
     
     def __init__(self, N, dx, Vx, hbar=1.0, m=1.0):
-        """Initialize propagator
-
-        The propagator propagates a wavefunction defined on a spatial grid.
-        The spatial grid spacing is assumed to be equal.
-        
-        Parameters
-        ----------
-        N : int
-            the number of spatial grid points
-        dx : float
-            grid spacing of the spatial grid
-        Vx : (N,) array-like
-            poential energy values on the spatial grid points
-        hbar : float
-            the Planck constant over 2pi
-        m : float
-            the mass of the particle
-        """
         
         # Check arguments
         if not isinstance(N, Integral) or not (N > 0):
@@ -73,3 +66,28 @@ class Time_Indep_Hamil_Propagator(object):
         for _ in range(Nt):
             tridiag_forward(_U, sf_arr, _sf_at_mid_time)
             tridiag_backward(_U_adj, sf_arr, _sf_at_mid_time)
+    
+    def propagate_to_ground_state(self, sf_arr, dt=None, Nt_max=5000, 
+                                  norm_thres=1e-14, Nt_per_iter=10):
+        
+        if dt is None: _dt = self.dx / 4.
+        else:
+            _dt = float(dt)
+            if not isinstance(_dt, Real) or not (_dt > 0):
+                _msg = "The `dt` should be real positive. Given: {}"
+                raise ValueError(_msg.format(dt))    
+        _imag_dt = -1.0j * _dt
+        
+        normalize_on_uniform_grid_1d_box(sf_arr, self.dx)
+        _sf_prev = sf_arr.copy()
+        
+        _max_iter = int(Nt_max / Nt_per_iter) + 1
+        for _i in range(_max_iter):
+            self.propagate(sf_arr, _imag_dt, Nt=Nt_per_iter)
+            normalize_on_uniform_grid_1d_box(sf_arr, self.dx)
+            _norm_sq_of_diff = norm_sq_on_uniform_grid_1d_box(
+                    sf_arr-_sf_prev, self.dx)
+            if _norm_sq_of_diff < norm_thres: break
+            _sf_prev = sf_arr.copy()
+        if _i >= _max_iter: raise Exception("Maximum iteration reached.")
+
