79a33175ad577e4317e597741b6e2fd2f65dda78
Ahn, Seongjin <jam45@naver.com>
2020-03-05 16:46:18 +0900

    add 1d box wavefunction

---
+++ tdse/propagator/box1d.py
@@ -0,0 +1,163 @@
+"""Module for wavefunction propagator"""
+
+from numbers import Integral
+
+from numpy import asarray
+
+from ._base import Wavefunction, Propagator
+
+
+class Wavefunction_Uniform_1D_Box(Wavefunction):
+    """
+    An object for specifying the wavefunction in a one-dimensional box
+    with uniform (i.e. constant grid spacing) grid
+    """
+    dim = 1
+
+    def __init__(self, N, dx):
+        """
+        Initialize the wavefunction
+        
+        Parameters
+        ----------
+        Nr : int
+            The number of grid points,
+            excluding the both ends where the wavefunction values are zero.
+        dr : float
+            The spacing for spatial grid. It should be a postive real number.
+        """
+        # Check and set arguments as members
+        if not isinstance(N, Integral) or not (N > 0):
+            _msg = "`N` should be a positive integer. Given: {}"
+            raise ValueError(_msg.format(N))
+        self.N = N
+        
+        if not (float(dx) > 0):
+            _msg = "`dx` should be a positive real number. Given: {}"
+            raise ValueError(_msg.format(dx))
+        self.dx = float(dx)
+
+        self.shape = (self.N,)
+
+    @staticmethod
+    def norm_sq(wf, dx):
+        _wf = asarray(wf)
+        _abs_sq_wf = np.real(np.conj(_wf) * _wf)
+        return dx * np.sum(_abs_sq_wf)
+
+
+# A propagator for the wavefunction in a one-dimensional box
+
+#def norm_sq_on_uniform_grid_1d_box(wf, dx):
+#    _abs_sq_wf = np.real(np.conj(wf) * wf)
+#    return dx * np.sum(_abs_sq_wf)
+#
+#def normalize_on_uniform_grid_1d_box(wf, dx):
+#    _norm_sq = norm_sq_on_uniform_grid_1d_box(wf, dx)
+
+#    wf[:] *= 1. / np.sqrt(_norm_sq)
+
+
+from numbers import Integral, Real
+
+import numpy as np
+
+from tdse.evol import get_D2_tridiag, get_M2_tridiag, mul_tridiag_and_diag
+from tdse.tridiag import tridiag_forward, tridiag_backward
+
+class Time_Indep_Hamil_Propagator(Propagator):
+    """A Propagator for a time-independent Hamiltonian"""
+
+    wf_class = Wavefunction_Uniform_1D_Box
+    
+    def __init__(self, N, dx, Vx, hbar=1.0, m=1.0):
+
+        self.wf = self.wf_class(N, dx)
+        for _attr in ("N","dx"):
+            setattr(self, _attr, getattr(self.wf, _attr))
+        
+        # Check arguments
+#        if not isinstance(N, Integral) or not (N > 0):
+#            _msg = "`N` should be a positive integer. Given: {}"
+#            raise TypeError(_msg.format(N))
+#        self.N = N
+#        
+#        if not (dx > 0):
+#            _msg = "`dx` should be positive. Given: {}"
+#            raise TypeError(_msg.format(dx))
+#        self.dx = dx
+            
+        try: _Vx = np.array(Vx, copy=False)
+        except:
+            _msg = "Failed to convert the given potential array: `Vx`"
+            raise Exception(_msg.format(Vx))
+        if _Vx.shape != (N,):
+            _msg = ("The potential array should have shape (N,)==({},)\n"
+                    "Given: {}")
+            raise TypeError(_msg.format(N, Vx))
+        self.Vx = _Vx
+        
+        if not (hbar > 0.0) or not (m > 0.0):
+            _msg = ("hbar and m (mass) should be positive\n"
+                    "Given hbar = {}, m = {}")
+            raise TypeError(_msg.format(hbar, m))
+        self.hbar, self.m = hbar, m
+        
+        # Construct tridiagonals for propagation
+        self.M2 = get_M2_tridiag(self.N)
+        self.D2 = get_D2_tridiag(self.N, self.dx)
+        _M2V = mul_tridiag_and_diag(self.M2, self.Vx)
+        self.M2H = -0.5*self.hbar**2/self.m * self.D2 + _M2V
+        
+        
+    def propagate(self, sf_arr, dt, Nt=1):
+        """Propagate the given state function by the given time interval"""
+        assert isinstance(sf_arr, np.ndarray) and sf_arr.dtype == np.complex
+        _sf_at_mid_time = np.empty_like(sf_arr)
+        _U = self.M2 - 0.5j*dt*self.M2H
+        _U_adj = self.M2 + 0.5j*dt*self.M2H
+        for _ in range(Nt):
+            tridiag_forward(_U, sf_arr, _sf_at_mid_time)
+            tridiag_backward(_U_adj, sf_arr, _sf_at_mid_time)
+
+    def propagate_to_ground_state(self, wf=None, dt=None, max_Nt=5000, 
+                                  Nt_per_iter=10, norm_thres=1e-13):
+        # Determine the timestep
+        _dt = dt
+        if dt is None: _dt = self.dx / 4.
+    
+        # Determine the wavefucntion
+        if wf is None: 
+            _wf = np.empty(self.wf.shape, dtype=np.complex)
+            _wf[:] = np.random.rand(*_wf.shape)
+        else: _wf = np.asarray(wf)
+
+        # Propagate to the ground state
+        _normalizer_args = (self.dx,)
+        super().propagate_to_ground_state(_wf, _dt, max_Nt, _normalizer_args, 
+                Nt_per_iter, norm_thres)
+    
+#    def propagate_to_ground_state(self, sf_arr, dt=None, Nt_max=5000, 
+#                                  norm_thres=1e-14, Nt_per_iter=10):
+#        
+#        if dt is None: _dt = self.dx / 4.
+#        else:
+#            _dt = float(dt)
+#            if not isinstance(_dt, Real) or not (_dt > 0):
+#                _msg = "The `dt` should be real positive. Given: {}"
+#                raise ValueError(_msg.format(dt))    
+#        _imag_dt = -1.0j * _dt
+#        
+#        normalize_on_uniform_grid_1d_box(sf_arr, self.dx)
+#        _sf_prev = sf_arr.copy()
+#        
+#        _max_iter = int(Nt_max / Nt_per_iter) + 1
+#        for _i in range(_max_iter):
+#            self.propagate(sf_arr, _imag_dt, Nt=Nt_per_iter)
+#            normalize_on_uniform_grid_1d_box(sf_arr, self.dx)
+#            _norm_sq_of_diff = norm_sq_on_uniform_grid_1d_box(
+#                    sf_arr-_sf_prev, self.dx)
+#            if _norm_sq_of_diff < norm_thres: break
+#            _sf_prev = sf_arr.copy()
+#        if _i >= _max_iter-1: raise Exception("Maximum iteration reached.")
+
